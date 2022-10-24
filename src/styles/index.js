7593ed3525153004913fe24bab42ffee514cc197
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-10-24 18:31:45 -0400

    Define index.js in styles for easy import/export

---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+// This is for easy exporting and importing
+// so that I don't have to import individual colors and fonts
+import * as Colors from './colors';
+import * as Typography from './typography';
+
+export {Typography, Colors};
+// Just one import line would suffice
