98ee230d0dead019f3525d9d7715b1a29204524e
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-10-24 18:14:57 -0400

    Define typography

---
+++ src/styles/typography.js
@@ -0,0 +1,8 @@
+// FONT FAMILY
+export const FONT_FAMILY_BOLD = 'Inter-Bold';
+export const FONT_FAMILY_EXTRA_BOLD = 'Inter-ExtraBold';
+export const FONT_FAMILY_LIGHT = 'Inter-Light';
+export const FONT_FAMILY_EXTRA_LIGHT = 'Inter-ExtraLight';
+export const FONT_FAMILY_MEDIUM = 'Inter-Medium';
+export const FONT_FAMILY_REGULAR = 'Inter-Regular';
+export const FONT_FAMILY_THIN = 'Inter-Thin';
