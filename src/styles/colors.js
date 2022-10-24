677f3baec4f65c44eaca399a5392c8a1258373f1
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-10-24 18:15:05 -0400

    Define colors

---
+++ src/styles/colors.js
@@ -0,0 +1,6 @@
+// Primary colors
+export const XIKETIC = '#11081E';
+export const REBECCA = '#5F3694';
+export const RACING = '#27452A';
+export const ORANGE = '#F3B045';
+export const BLUE = '#5D9FE6';
