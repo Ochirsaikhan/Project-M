aadc4b9d62afafdb632c82e9b72637eb08ff8c79
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-11-01 15:27:17 -0400

    Style the SearchScreen component

--- src/scenes/search/SearchScreen.js
+++ src/scenes/search/SearchScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
+import {Colors, Typography} from '../../styles';
+import SearchBar from '../../components/atoms/SearchBar';
 
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>This is Search Screen!</Text>
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>This is Search Screen!</Text> */}
+      <SearchBar style={styles.text} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.XIKETIC,
+  },
+  text: {
+    flex: 1,
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
 
 export default SearchScreen;
