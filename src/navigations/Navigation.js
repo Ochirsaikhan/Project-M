a86855964ae144fd26db5933cf660b1396e18761
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-11-03 14:27:54 -0400

    Change bottom tab icons
    Using Ionicons, change each tab's icons and set tab bar active color to
    rebecca for aesthetics. Learned a few things about tabBarIcon option.

--- src/navigations/Navigation.js
+++ src/navigations/Navigation.js
@@ -2,6 +2,8 @@
 import HomeScreen from '../scenes/home/HomeScreen';
 import LibraryScreen from '../scenes/library/LibraryScreen';
 import SearchScreen from '../scenes/search/SearchScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Colors} from '../styles';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
@@ -9,15 +11,36 @@ const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Library" component={LibraryScreen} />
+    <Tab.Navigator screenOptions={{tabBarActiveTintColor: Colors.REBECCA}}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Library"
+        component={LibraryScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="musical-notes" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Search"
         component={SearchScreen}
         // Screen component accepts `options` prop which is either an object or
         // a function that returns an object, that contains various configuration options.
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search-outline" size={size} color={color} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
