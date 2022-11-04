c389e08750b921ce422daa28b1c96f00dd29a15c
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-11-04 00:08:10 -0400

    Add states for user input text and user final text
    userInputText is for showing recommended text while finalText is for
    searching videos when user submits their search. I might change it later
    to use only one state

--- src/scenes/search/SearchScreen.js
+++ src/scenes/search/SearchScreen.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Colors, Typography} from '../../styles';
 import SearchBar from '../../components/atoms/SearchBar';
 
 const SearchScreen = () => {
+  // Whenever we call the state setter function, the SearchScreen component
+  // will re-render, and useState will give us the next value of state. That's why
+  // we can update state even though it's defined as const.
+  const [userInputText, setUserInputText] = useState();
+  const [finalText, setFinalText] = useState();
+
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}>This is Search Screen!</Text> */}
-      <SearchBar style={styles.text} />
+      <SearchBar
+        userInputText={userInputText}
+        setUserInputText={setUserInputText}
+        setFinalText={setFinalText}
+      />
+      <Text style={{color: 'white'}}>{userInputText}</Text>
+      <Text style={{color: 'white'}}>Welcome, {finalText}</Text>
     </View>
   );
 };
@@ -16,13 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.XIKETIC,
-  },
-  text: {
-    flex: 1,
-    fontFamily: Typography.FONT_FAMILY_REGULAR,
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center',
   },
 });
 
