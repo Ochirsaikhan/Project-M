5b4b92b80982db68cd6f1cfabeae222ee0daea5e
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-11-04 00:12:04 -0400

    Handle onChangeText and Submit by using state
    setters from parent component. This conforms to react's philosophy of
    one-way-data-flow that keeps everything modular and fast

--- src/components/atoms/SearchBar.js
+++ src/components/atoms/SearchBar.js
@@ -2,11 +2,20 @@ import React from 'react';
 import {TextInput, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Colors, Typography} from '../../styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const SearchBar = () => {
+const SearchBar = props => {
   return (
     <SafeAreaView>
-      <TextInput style={styles.searchBar} placeholder="Search" />
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search"
+        // value={props.userInputText}
+        onChangeText={userText => props.setUserInputText(userText)}
+        onSubmitEditing={finalText =>
+          props.setFinalText(finalText.nativeEvent.text)
+        }
+      />
     </SafeAreaView>
   );
 };
